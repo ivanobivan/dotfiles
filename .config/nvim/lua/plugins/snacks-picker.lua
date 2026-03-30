749ffbd448bec28720e75821f9e5fb2c9c89956b
ivanobivan <ikolesov2015@gmail.com>
2026-03-30 15:12:19 +0300

    feat(nvim): add function for copy same dir with rename

--- .config/nvim/lua/plugins/snacks-picker.lua
+++ .config/nvim/lua/plugins/snacks-picker.lua
@@ -6,10 +6,56 @@ return {
                 preview = false,
                 ignored = false,
                 hidden = false,
+                actions = {
+                    explorer_copy_default = function(picker, item)
+                        if not item then
+                            return
+                        end
+                        local Tree = require("snacks.explorer.tree")
+                        local actions = require("snacks.explorer.actions")
+                        local uv = vim.uv or vim.loop
+                        ---@type string[]
+                        local paths = vim.tbl_map(Snacks.picker.util.path, picker:selected())
+                        -- Copy selection
+                        if #paths > 0 then
+                            local dir = picker:dir()
+                            Snacks.picker.util.copy(paths, dir)
+                            picker.list:set_selected() -- clear selection
+                            Tree:refresh(dir)
+                            Tree:open(dir)
+                            actions.update(picker, { target = dir })
+                            return
+                        end
+                        Snacks.input({
+                            prompt = "Copy to",
+                            default = vim.fn.fnamemodify(item.file, ":t"),
+                        }, function(value)
+                            if not value or value:find("^%s$") then
+                                return
+                            end
+                            local dir = vim.fs.dirname(item.file)
+                            local to = svim.fs.normalize(dir .. "/" .. value)
+                            if uv.fs_stat(to) then
+                                Snacks.notify.warn("File already exists:\n- `" .. to .. "`")
+                                return
+                            end
+                            Snacks.picker.util.copy_path(item.file, to)
+                            Tree:refresh(vim.fs.dirname(to))
+                            actions.update(picker, { target = to })
+                        end)
+                    end,
+                },
                 sources = {
                     explorer = {
                         hidden = true,
                         ignored = true,
+                        win = {
+                            list = {
+                                keys = {
+                                    ["C"] = "explorer_copy_default",
+                                },
+                            },
+                        },
                     },
                 },
             },
