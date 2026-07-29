7fc35f0d8b7971a47f8eb6868d164dc5f684c0f1
ivanobivan <ikolesov2015@gmail.com>
2026-07-29 11:26:03 +0300

    feat(fish): pers vars

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,9 +2,15 @@
 fish_add_path /opt/nvim-linux-x86_64/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.local/share/coursier/bin
+fish_add_path $HOME/perl5/bin
 
 set -Ux EDITOR nvim
 set -Ux VISUAL nvim
+
+set -gx PERL5LIB $HOME/perl5/lib/perl5
+set -gx PERL_LOCAL_LIB_ROOT $HOME/perl5
+set -gx PERL_MB_OPT "--install_base /home/i.kolesov/perl5"
+set -gx PERL_MM_OPT "INSTALL_BASE=$HOME/perl5"
 
 set fish_greeting
 set -U fish_color_autosuggestion 7f848e
